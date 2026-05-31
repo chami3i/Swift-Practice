c9aaa6b924b1b6c44779a93678fa6859d2dfd6e9
chami3i <hongcm2002@naver.com>
2026-05-31 16:15:09 +0900

    Create TrailAnalyzer app scaffold

---
+++ TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by chaem on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
